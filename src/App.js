7bfe8bbf7a466a4ac0d3d5fb4300ddc85d2772e3
Mufaddal Motorwala <mufaddal.motorwala@gmail.com>
2018-03-17 19:03:04 +0400

    Update.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     this.state = {
       lat: null,
       lon: null,
-      weather: null,
+      desc: null,
       icon: null,
       temp: null,
       wind: null,
@@ -32,11 +32,11 @@ class App extends Component {
 
     axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       currentComponent.setState({ 
         lat: response.data.coord.lat,
         lon: response.data.coord.lon,
-        weather: response.data.weather[0].description,
+        desc: response.data.weather[0].description,
         icon: response.data.weather[0].icon,
         temp: response.data.main.temp,
         wind: response.data.wind.speed,
@@ -52,15 +52,23 @@ class App extends Component {
     });
 
   }
+  // kelvinToTemp = () => {
+  //   this.setState({ temp: this.state.temp - 273.15 });
+  // }
   render() {
-    
     return (
       <div className="App">
         
          {/* <header className="App-header"> */}
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="weather__city">{this.state.city}</h1>
-          
+          <h1 className="weather__city">{this.state.city} {this.state.country}</h1>
+          <div>{this.state.desc}</div>
+          <div>{this.state.temp}</div>
+          <div>{this.state.icon}</div>
+          <div>{this.kelvinToTemp}</div>
+          <div>{this.state.wind}</div>
+          <div>{this.state.sunrise}</div>
+          <div>{this.state.sunset}</div>
           {/* <h1 className="App-title">{this.state.lat}</h1> */}
         {/* </header> */}
         {/* <p className="App-intro"> */}
